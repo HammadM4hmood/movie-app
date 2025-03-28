fa2a5266055f9951d095192bb9e37c1a448d74e0
Hammad Mahmood <109633500+HammadM4hmood@users.noreply.github.com>
2025-03-28 03:17:27 -0600

    Add files via upload

---
+++ movie-app/app/api/movies/route.js
@@ -0,0 +1,37 @@
+import client from "../../libs/prismadb";
+import { NextResponse } from "next/server";
+
+// url: http://localhost:3000/api/movies
+
+export const POST = async (req) => {
+  try {
+    const body = await req.json();
+    const { title, actors, releaseYear } = body;
+    const newMovie = await client.movie.create({
+      data: {
+        title,
+        actors,
+        releaseYear,
+      },
+    });
+    return NextResponse.json(newMovie);
+  } catch (error) {
+    console.error("POST /api/movies error:", error);
+    return NextResponse.json(
+      { message: "Error creating movie", error },
+      { status: 500 }
+    );
+  }
+};
+
+export const GET = async () => {
+  try {
+    const movies = await client.movie.findMany();
+    return NextResponse.json(movies);
+  } catch (error) {
+    return NextResponse.json(
+      { status: 500 },
+      { message: "Error getting movies", error }
+    );
+  }
+};
---
+++ movie-app/app/page.js
@@ -0,0 +1,20 @@
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/movies', {cache: "no-cache"})
+  if(!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+  return res.json()
+}
+const page = async() =>{
+
+  const posts = await getData()
+  console.log(posts)
+  return (
+    
+    <main className="flex min-h-screen flex-col justify-between p-24">
+    
+    </main>
+  );
+}
+
+export default page;
